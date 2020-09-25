8f888b4360fb297c25d5b98afe436ea84318b6ab
willnilges <wdn5796@rit.edu>
2020-09-24 21:31:08 -0400

    Demo new RustyHUD

--- src/main.rs
+++ src/main.rs
@@ -11,101 +11,61 @@ fn main() {
     let mut windows: std::collections::HashMap<String, (WINDOW, WindowData)> = HashMap::new();
 
     launch();
-
-    // // Use WindowData structs to keep track of the windows you want to create
-
-    // // Create one window
-    // let window = WindowData {
-    //         id: String::from("win1"),
-    //         content: WindowContent::Text,
-    //         message: String::from("Hello!!!"),
-    //         style: WindowStyle::Plain,
-    //         ticks: None,
-    //         x_pos: 10,
-    //         y_pos: 10,
-    //         width: 10,
-    //         height: 10,
-    //         priority: false
-    //     };
-
-    // open_win(window, &mut windows); // Open the window.
-    // thread::sleep(time::Duration::from_millis(1000)); // Sleep a bit.
-
-    // // Create another window.
-    // let mod_window = WindowData {
-    //         id: String::from("win2"),
-    //         content: WindowContent::Text,
-    //         message: String::from("I'm gonna change this window."),
-    //         style: WindowStyle::Plain,
-    //         ticks: None,
-    //         x_pos: 25,
-    //         y_pos: 10,
-    //         width: 10,
-    //         height: 10,
-    //         priority: false
-    //     };
-
-    // open_win(mod_window, &mut windows); // Open the window
-    // thread::sleep(time::Duration::from_millis(1000));
-
-    // change_window_message("win2", "I HAVE CHANGED YOUUUUUU!!!!", &mut windows);
-
-    // thread::sleep(time::Duration::from_millis(1000));
-
-    // change_window_style("win2", WindowStyle::Bold, &mut windows);
-
-    // thread::sleep(time::Duration::from_millis(1000));
-
-    // // How about a progressbar?
-
-    // let progress = WindowData {
-    //     id: String::from("BRUH-ometer"),
-    //     content: WindowContent::ProgressBar,
-    //     message: String::from("0/20"),
-    //     style: WindowStyle::Plain,
-    //     ticks: Some(String::from("&")),
-    //     x_pos: 5,
-    //     y_pos: 5,
-    //     width: 30,
-    //     height: 1,
-    //     priority: false
-    // };
-
-    // open_win(progress, &mut windows); // Open the window
-
-    // thread::sleep(time::Duration::from_millis(1000));
-
-    // for i in 0..21 {
-    //     change_window_message("BRUH-ometer", format!("{}/20", i).as_str(), &mut windows);
-    //     thread::sleep(time::Duration::from_millis(100));
-    // }
-
-    // thread::sleep(time::Duration::from_millis(1000));
-
-    // change_window_ticks("BRUH-ometer", "O", &mut windows);
-
-    // thread::sleep(time::Duration::from_millis(1000));
-
-    //Okay here comes the new shit.
-
     let mut testwin2 = WindowData::new()
         .with_id("yeetussss")
         .with_message("Hallo")
-        .with_position(30, 30)
+        .with_position(20, 20)
         .with_dimensions(20, 20)
         .build();
-
+        
     testwin2.open();
     
-    thread::sleep(time::Duration::from_millis(3000));
+    thread::sleep(time::Duration::from_millis(500));
 
     testwin2.close();
 
-    thread::sleep(time::Duration::from_millis(3000));
+    thread::sleep(time::Duration::from_millis(500));
 
     testwin2.open();
 
-    thread::sleep(time::Duration::from_millis(3000));
+    thread::sleep(time::Duration::from_millis(500));
+
+    testwin2.mv(30, 13);
+
+    let mut otherwin = WindowData::new()
+        .with_id("Changewin")
+        .with_message("I'm gonna change this.")
+        .with_position(10, 10)
+        .with_dimensions(13, 13)
+        .build();
+
+    otherwin.open();
+
+    thread::sleep(time::Duration::from_millis(500));
+
+    testwin2.resize(8, 8);
+
+    otherwin.message = String::from("I changed it!");
+    otherwin.redraw();
+
+    thread::sleep(time::Duration::from_millis(2000));
+
+    let mut progressbaryeetus = WindowData::new()
+        .with_id("Bruv-o-meter")
+        .with_content(WindowContent::ProgressBar)
+        .with_message("0/20")
+        .with_ticks("#")
+        .with_position(15, 3)
+        .with_dimensions(30, 1)
+        .build();
+
+    progressbaryeetus.open();
+
+    for i in 0..21 {
+        progressbaryeetus.message = format!("{}/20", i);
+        progressbaryeetus.redraw();
+        thread::sleep(time::Duration::from_millis(100));
+    }
 
     // Clean up.
     endwin();
